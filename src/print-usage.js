e6c3896fce8bbe83dfeaf25262afbcf4039fa1bd
Phillip Gibson <gibson.phillip.j@gmail.com>
2017-10-31 14:18:17 -0400

    Split into modules

---
+++ src/print-usage.js
@@ -0,0 +1,12 @@
+const log = console.log
+
+module.exports = (cli, commands, version) => {
+  log()
+  log(`  v${version}`)
+  log()
+  log(`  Usage: ${cli} <command>`)
+  log()
+  log('  Commands:')
+  Object.keys(commands).forEach((cmd) => log(`    ${cmd} ${commands[cmd].args}`))
+  log()
+}
